debe0d74caaf0c854a6d761bad966ad0c854009c
U.amazing <1259642433@qq.com>
2020-10-09 15:58:01 +0800

    update trans>hls&&dash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const NodeMediaServer = require('node-media-server');
 
 const config = {
+  logType: 2,
   rtmp: {
     port: 1935,
     chunk_size: 60000,
@@ -10,8 +11,21 @@ const config = {
   },
   http: {
     port: 8000,
+    mediaroot: './media',
     allow_origin: '*'
-  }
+  },
+  trans: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  },
 };
 
 var nms = new NodeMediaServer(config)
