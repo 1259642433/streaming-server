1078a23aff8a0b40a4d02a0855f6b83e6d3ec957
U.amazing <1259642433@qq.com>
2021-01-25 11:38:38 +0800

    feat:创建并提交服务器版本分支

--- app.js
+++ app.js
@@ -10,24 +10,22 @@ const config = {
     ping_timeout: 30
   },
   http: {
-    port: 9001,
+    port: 8000,
     mediaroot: './media',
     allow_origin: '*'
   },
-  trans: { // 这里参数是trans参数，不是relay参数，relay参数中配置hls无效
-    ffmpeg: './ffmpeg.exe',//指明FFmpeg位置
+  trans: {
+    ffmpeg: '/usr/bin/ffmpeg',
     tasks: [
-        {
-            app: 'live',
-            ac: 'acc',
-            vc: 'libx264',
-            hls: true,
-            hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-            dash: true,
-            dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-        }
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
     ]
-  }
+  },
 };
 
 var nms = new NodeMediaServer(config)
