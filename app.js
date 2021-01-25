aface749d1d839fc50a591bc2e736dac3deafc49
U.amazing <1259642433@qq.com>
2021-01-25 11:22:29 +0800

    feat:接入ffmpeg转换格式来传输hls流,更新依赖

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const config = {
     port: 1935,
     chunk_size: 60000,
     gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
+    ping: 60,
+    ping_timeout: 30
   },
   http: {
-    port: 8000,
+    port: 9001,
+    mediaroot: './media',
     allow_origin: '*'
+  },
+  trans: { // 这里参数是trans参数，不是relay参数，relay参数中配置hls无效
+    ffmpeg: './ffmpeg.exe',//指明FFmpeg位置
+    tasks: [
+        {
+            app: 'live',
+            ac: 'acc',
+            vc: 'libx264',
+            hls: true,
+            hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+            dash: true,
+            dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+        }
+    ]
   }
 };
 
